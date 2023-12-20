6c1ea9f5f7dd36882118443c6e128e2a2196bdeb
dariatur <dashatur100500@gmail.com>
2023-12-20 19:30:25 +0100

    feat: did cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,4 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -20,13 +19,84 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  isDirect = true;
+
+  constructor(isDirect) {
+    if(arguments.length === 1){
+        this.isDirect = arguments[0];
+    } else {
+        this.isDirect = true;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(word, key) {
+    if(arguments.length < 2 || typeof arguments[0] === 'undefined'){
+      throw new Error('Incorrect arguments!');
+    }
+    let longKey = '';
+    let i = 0;
+    let cipher = '';
+    while (longKey.length < word.length) {
+      longKey += key[i];
+      i++;
+      if(i === key.length){
+        i = 0;
+      }
+    }
+
+    let k = 0;
+    for (let i = 0; i <word.length; i++) {
+      if(this.alphabet.indexOf(word[i].toUpperCase())!== -1){
+        const index = (this.alphabet.length+this.alphabet.indexOf(word[i].toUpperCase())+this.alphabet.indexOf(longKey[k].toUpperCase())) % this.alphabet.length;
+        cipher += this.alphabet[index];
+        k++;
+      } else {
+        cipher += word[i];
+      }
+      
+    }
+
+    if(this.isDirect){
+      return cipher;
+    } else {
+      return cipher.split('').reverse().join('');
+    }
+  }
+
+  decrypt(cipher, key) {
+    if(arguments.length < 2 || typeof arguments[0] === 'undefined'){
+      throw new Error('Incorrect arguments!');
+    }
+
+    let longKey = '';
+    let i = 0;
+    let word = '';
+    while (longKey.length < cipher.length) {
+      longKey += key[i];
+      i++;
+      if(i === key.length){
+        i = 0;
+      }
+    }
+
+    let k = 0;
+    for (let i = 0; i <cipher.length; i++) {
+      if(this.alphabet.indexOf(cipher[i].toUpperCase())!== -1){
+        const index = (this.alphabet.length+this.alphabet.indexOf(cipher[i].toUpperCase())-this.alphabet.indexOf(longKey[k].toUpperCase())) % this.alphabet.length;
+        word += this.alphabet[index];
+        k++;
+      } else {
+        word += cipher[i];
+      }
+      
+    }
+
+    if(this.isDirect){
+      return word;
+    } else {
+      return word.split('').reverse().join('');
+    }
   }
 }
 
